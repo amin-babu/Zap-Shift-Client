94df86b33ce8f8454514db83c8cbbadc11125028
aminbabu12 <badrulaminbabu@gmail.com>
2025-11-19 12:13:21 +0600

    regiters added

---
+++ src/Pages/Auth/Register/Register.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const Register = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const handleRegistrarion = (data) => {
+    console.log('Testing Data', data);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(handleRegistrarion)}>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" {...register('email', { required: true })} className="input" placeholder="Email" />
+          {errors.email?.type === 'required' && <p className='text-red-500'>Email is required</p>}
+          <label className="label">Password</label>
+          <input type="password" {...register('password', {
+            required: true,
+            minLength: 6,
+            pattern: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&]).+$/
+          })} className="input" placeholder="Password" />
+          {errors.password?.type === 'required' && <p className='text-red-500'>Password is required</p>}
+          {errors.password?.type === 'minLength' && <p className='text-red-500'>Password should be at least 6 length</p>}
+          {errors.password?.type === 'pattern' && <p className='text-red-500'>Password should be at one upper, lower and special character</p>}
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
